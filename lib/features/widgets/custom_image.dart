fd5f9f12ca03930c239c637cf0d040ea87769312
Manuel Duarte  <carlosd99m@gmail.com>
2022-11-11 14:50:18 -0600

    feat: create splash-screen (#4)
    * feat: create splash-screen
    
    * feat: creates routes
    
    * create onboarding
    
    * add commons widget

---
+++ lib/features/widgets/custom_image.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CustomImage extends StatelessWidget {
+
+  const CustomImage({
+    super.key,
+    this.imageFile,
+    this.imageUrl,
+    this.boxFit = BoxFit.cover,
+  });
+
+  final File? imageFile;
+  final String? imageUrl;
+  final BoxFit boxFit;
+
+  @override
+  Widget build(BuildContext context) {
+    if (imageFile != null) {
+      return Image.file(
+        imageFile!,
+        fit: boxFit,
+      );
+    } else if (imageUrl?.isNotEmpty == true) {
+      return CachedNetworkImage(
+        imageUrl: imageUrl!,
+        progressIndicatorBuilder: (_, __, progress) =>
+            CircularProgressIndicator(value: progress.progress),
+        errorWidget: (_, __, ___) => const Icon(Icons.error),
+        fit: boxFit,
+      );
+    }
+    return const ColoredBox(color: Colors.grey);
+  }
+}
